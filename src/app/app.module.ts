17f55c4c1cfe161aaed935070d335666a4f1932d
parinpond <parinpond.lag@gmail.com>
2021-09-28 22:27:10 +0700

    (fix): syntax

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ProductComponent } from './product/product.component';
 import { RouterModule } from '@angular/router';
-
 @NgModule({
   declarations: [
     AppComponent,
